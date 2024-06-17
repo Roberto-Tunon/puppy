f6d152461ab6906d4cf0b4da1dfccba028484b1e
robe <ROBE@xxxlgroup.com>
2024-06-17 13:21:24 +0200

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Roberto-Tunon/puppy
+
+go 1.22.3
---
+++ puppy.go
@@ -0,0 +1,9 @@
+package puppy
+
+func Bark() string {
+	return "Woooof!!!!"
+}
+
+func Barks() string {
+	return "Woooof!!!! Woooof!!!! Woooof!!!!"
+}
